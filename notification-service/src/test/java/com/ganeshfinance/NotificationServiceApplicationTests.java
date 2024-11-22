4db793c4fd838092b2289ddaeaf97a512b4502a2
Avinash Andhale <avinashandhale999@gmail.com>
2024-11-22 12:50:08 +0530

    initial microservice

---
+++ notification-service/src/test/java/com/ganeshfinance/NotificationServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ganeshfinance;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NotificationServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
